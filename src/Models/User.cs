ed6de8dfd0e887a374e312384cfcb201e71f8991
alvesjaov <victoralves169@outlook.com>
2024-11-30 14:55:46 -0300

    Renomear Users.cs para User.cs e alterar tipo de Id para string com validações

--- src/Models/User.cs
+++ src/Models/User.cs
@@ -1,10 +1,13 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace myBURGUERMANIA_API.Models
 {
     public class User
     {
-        public int Id { get; set; }
+        [Required]
+        [StringLength(36)]
+        public string Id { get; set; } = string.Empty; // Definir comprimento máximo
         
         [Required]
         [StringLength(50)]
