109af94465077a5e04bfb75133726baf62b0cf26
alvesjaov <victoralves169@outlook.com>
2024-11-30 15:50:59 -0300

    Atualizar tipo de BirthDate para DateTime na tabela Users e ajustar migrações correspondentes

---
+++ Migrations/20241130184519_UpdateBirthDateToDateTime.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace myBURGUERMANIA_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBirthDateToDateTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "Users",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BirthDate",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime");
+        }
+    }
+}
