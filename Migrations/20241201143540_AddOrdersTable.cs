205212e745f8040ff355b92d15baf3b7b1bd92dc
alvesjaov <victoralves169@outlook.com>
2024-12-01 11:48:38 -0300

    Adicionar tabela de pedidos com propriedades e migrações correspondentes

---
+++ Migrations/20241201143540_AddOrdersTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace myBURGUERMANIA_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrdersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(36)", nullable: false),
+                    UserId = table.Column<string>(type: "varchar(36)", nullable: false),
+                    ProductIds = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Status = table.Column<string>(type: "varchar(20)", nullable: false),
+                    TotalValue = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
