2d8020528bcdcf05c8a7a41ce70997bb3f8a0612
alvesjaov <victoralves169@outlook.com>
2024-12-02 14:43:45 -0300

    Adicionar tabela Status e atualizar relacionamento com pedidos

---
+++ Migrations/20241202165508_AddStatusesTable.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace myBURGUERMANIA_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StatusId",
+                table: "Orders",
+                type: "varchar(36)",
+                maxLength: 36,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Statuses",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(36)", maxLength: 36, nullable: false),
+                    Name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statuses", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_StatusId",
+                table: "Orders",
+                column: "StatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Statuses_StatusId",
+                table: "Orders",
+                column: "StatusId",
+                principalTable: "Statuses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Statuses_StatusId",
+                table: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Statuses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_StatusId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Orders",
+                type: "varchar(20)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
