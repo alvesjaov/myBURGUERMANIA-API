2b58c0c27d6252f771d5dcd95657267d9dbbaf83
alvesjaov <victoralves169@outlook.com>
2024-12-04 23:24:28 -0300

    Adicionar coluna UserId à tabela SelectedProducts na migração

---
+++ Migrations/20241205021756_AddUserIdToSelectedProducts.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace myBURGUERMANIA_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToSelectedProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "SelectedProducts",
+                type: "longtext",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "SelectedProducts");
+        }
+    }
+}
